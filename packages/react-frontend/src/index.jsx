16ee4c4d18a47a2388d347de63bf1442405cd607
kct417 <ctran156@calpoly.edu>
2024-05-15 23:44:13 -0700

    rename main.jsx to index.jsx to match formatting. navigate to home page on login/register

--- packages/react-frontend/src/index.jsx
+++ packages/react-frontend/src/index.jsx
@@ -1,11 +1,7 @@
-// import React from 'react';
 import ReactDOMClient from 'react-dom/client';
 import App from './App';
 
 const container = document.getElementById('root');
-
-// Create a root
 const root = ReactDOMClient.createRoot(container);
 
-// Initial render:
 root.render(<App />);
