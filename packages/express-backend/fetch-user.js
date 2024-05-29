08d5de2915f8a3f6fbdaaadf05d07aceb6001c3a
kct417 <ctran156@calpoly.edu>
2024-05-29 15:32:02 -0700

    Routes

--- packages/express-backend/fetch-user.js
+++ packages/express-backend/fetch-user.js
@@ -32,33 +32,33 @@ export const createDivider = (req, res) => {
 		});
 };
 
+export const deleteDivider = (req, res) => {
+	const { username } = req.params;
+	const { divider } = req.body;
+	updateDividers(username, divider, 'pull')
+		.then(() => res.status(204).send())
+		.catch((error) => {
+			console.log(error);
+			res.status(500).send(error);
+		});
+};
+
+export const setDivider = (req, res) => {
+	const { username } = req.params;
+	const { divider } = req.body;
+	updateDividers(username, divider, 'set')
+		.then(() => res.status(204).send())
+		.catch((error) => {
+			console.log(error);
+			res.status(500).send(error);
+		});
+};
+
 export const createFolder = (req, res) => {
 	const { username, dividerName } = req.params;
 	const { folder } = req.body;
 	updateFolders(username, dividerName, folder, 'push')
 		.then((result) => res.status(201).send(result))
-		.catch((error) => {
-			console.log(error);
-			res.status(500).send(error);
-		});
-};
-
-export const createTask = (req, res) => {
-	const { username, dividerName, folderName } = req.params;
-	const { task } = req.body;
-	updateTasks(username, dividerName, folderName, task, 'push')
-		.then((result) => res.status(201).send(result))
-		.catch((error) => {
-			console.log(error);
-			res.status(500).send(error);
-		});
-};
-
-export const deleteDivider = (req, res) => {
-	const { username } = req.params;
-	const { divider } = req.body;
-	updateFolders(username, divider, 'pull')
-		.then(() => res.status(204).send())
 		.catch((error) => {
 			console.log(error);
 			res.status(500).send(error);
@@ -76,6 +76,28 @@ export const deleteFolder = (req, res) => {
 		});
 };
 
+export const setFolder = (req, res) => {
+	const { username, dividerName } = req.params;
+	const { folder } = req.body;
+	updateFolders(username, dividerName, folder, 'set')
+		.then(() => res.status(204).send())
+		.catch((error) => {
+			console.log(error);
+			res.status(500).send(error);
+		});
+};
+
+export const createTask = (req, res) => {
+	const { username, dividerName, folderName } = req.params;
+	const { task } = req.body;
+	updateTasks(username, dividerName, folderName, task, 'push')
+		.then((result) => res.status(201).send(result))
+		.catch((error) => {
+			console.log(error);
+			res.status(500).send(error);
+		});
+};
+
 export const deleteTask = (req, res) => {
 	const { username, dividerName, folderName } = req.params;
 	const { task } = req.body;
@@ -87,33 +109,11 @@ export const deleteTask = (req, res) => {
 		});
 };
 
-export const setDivider = (req, res) => {
-	const { username } = req.params;
-	const { divider } = req.body;
-	updateDividers(username, divider, 'set')
-		.then(() => res.status(200).send())
-		.catch((error) => {
-			console.log(error);
-			res.status(500).send(error);
-		});
-};
-
-export const setFolder = (req, res) => {
-	const { username, dividerName } = req.params;
-	const { folder } = req.body;
-	updateFolders(username, dividerName, folder, 'set')
-		.then(() => res.status(200).send())
-		.catch((error) => {
-			console.log(error);
-			res.status(500).send(error);
-		});
-};
-
 export const setTask = (req, res) => {
 	const { username, dividerName, folderName } = req.params;
 	const { task } = req.body;
 	updateTasks(username, dividerName, folderName, task, 'set')
-		.then(() => res.status(200).send())
+		.then(() => res.status(204).send())
 		.catch((error) => {
 			console.log(error);
 			res.status(500).send(error);
@@ -123,12 +123,12 @@ export const setTask = (req, res) => {
 export default {
 	getUser,
 	createDivider,
-	createFolder,
-	createTask,
 	deleteDivider,
-	deleteFolder,
-	deleteTask,
 	setDivider,
+	createFolder,
+	deleteFolder,
 	setFolder,
+	createTask,
+	deleteTask,
 	setTask,
 };
