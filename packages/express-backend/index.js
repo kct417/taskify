728c14d1dd94275221be6a1aa2b2593303aa0715
kct417 <ctran156@calpoly.edu>
2024-05-29 13:41:11 -0700

    Update routes and Convert to Bootstrap

--- packages/express-backend/index.js
+++ packages/express-backend/index.js
@@ -15,36 +15,44 @@ app.use(express.json());
 app.post('/signup', registerUser);
 app.post('/login', loginUser);
 
-app.get('/:user', authenticateUser, User.getUser);
+app.get('/:username', authenticateUser, User.getUser);
 
-app.post('/:user', authenticateUser, User.createDivider);
-app.post('/:user/:divider', authenticateUser, User.createFolder);
-app.post('/:user/:divider/:folder', authenticateUser, User.createTask);
+app.post('/:username', authenticateUser, User.createDivider);
+app.post('/:username/:dividerName', authenticateUser, User.createFolder);
+app.post(
+	'/:username/:dividerName/:folderName',
+	authenticateUser,
+	User.createTask,
+);
 
-app.delete('/:user', authenticateUser, User.deleteDivider);
-app.delete('/:user/:divider', authenticateUser, User.deleteFolder);
-app.delete('/:user/:divider/:folder', authenticateUser, User.deleteTask);
+app.delete('/:username', authenticateUser, User.deleteDivider);
+app.delete('/:username/:dividerName', authenticateUser, User.deleteFolder);
+app.delete(
+	'/:username/:dividerName/:folderName',
+	authenticateUser,
+	User.deleteTask,
+);
 
-app.put('/:user', authenticateUser, User.setDivider);
-app.put('/:user/:divider', authenticateUser, User.setFolder);
-app.put('/:user/:divider/:folder', authenticateUser, User.setTask);
+app.put('/:username', authenticateUser, User.setDivider);
+app.put('/:username/:dividerName', authenticateUser, User.setFolder);
+app.put('/:username/:dividerName/:folderName', authenticateUser, User.setTask);
 
 // Routes without authentication for testing
 // Overrides previous route due to redeclaration after
 
-app.get('/:user', User.getUser);
+app.get('/:username', User.getUser);
 
-app.post('/:user', User.createDivider);
-app.post('/:user/:divider', User.createFolder);
-app.post('/:user/:divider/:folder', User.createTask);
+app.post('/:username', User.createDivider);
+app.post('/:username/:dividerName', User.createFolder);
+app.post('/:username/:dividerName/:folderName', User.createTask);
 
-app.delete('/:user', User.deleteDivider);
-app.delete('/:user/:divider', User.deleteFolder);
-app.delete('/:user/:divider/:folder', User.deleteTask);
+app.delete('/:username', User.deleteDivider);
+app.delete('/:username/:dividerName', User.deleteFolder);
+app.delete('/:username/:dividerName/:folderName', User.deleteTask);
 
-app.put('/:user', User.setDivider);
-app.put('/:user/:divider', User.setFolder);
-app.put('/:user/:divider/:folder', User.setTask);
+app.put('/:username', User.setDivider);
+app.put('/:username/:dividerName', User.setFolder);
+app.put('/:username/:dividerName/:folderName', User.setTask);
 
 app.listen(port, () => {
 	console.log(`Example app listening at ${API_PREFIX}`);
