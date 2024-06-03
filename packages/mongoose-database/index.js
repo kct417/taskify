8865ff36e2298f07c23df1322b258fddf6923600
Joshua Lowe <jlowe5681@gmail.com>
2024-06-03 15:22:22 -0700

    Removed index.js changes

--- packages/mongoose-database/index.js
+++ packages/mongoose-database/index.js
@@ -7,8 +7,6 @@ config({ path: ENV_PATH });
 
 mongoose.set('debug', true);
 
-console.log('MONGODB_URI:', process.env.MONGODB_URI);
-
 mongoose
 	.connect(process.env.MONGODB_URI, {})
 	.catch((error) => console.log(error));
