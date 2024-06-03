bb438b44c594391f05f56e14ce01a0a77fda2139
kct417 <ctran156@calpoly.edu>
2024-06-03 13:51:59 -0700

    Backend ordering to maintain order in frontend

--- packages/mongoose-database/models/user.js
+++ packages/mongoose-database/models/user.js
@@ -1,30 +1,37 @@
 import mongoose from 'mongoose';
 
 const taskSchema = new mongoose.Schema({
-	taskName: { type: String, required: true },
-	description: { type: String },
-	dueDate: { type: Date },
-	completed: { type: Boolean, default: false },
+	taskName: { type: String, trim: true, required: true },
+	description: { type: String, trim: true },
+	dueDate: { type: Date, trim: true },
+	completed: { type: Boolean, trim: true, default: false },
+	ordering: { type: Number, trim: true },
+	created: { type: Date, trim: true, default: Date.now },
 });
 
 const folderSchema = new mongoose.Schema({
-	folderName: { type: String, required: true },
-	description: { type: String },
+	folderName: { type: String, trim: true, required: true },
+	description: { type: String, trim: true },
 	tasks: [taskSchema],
+	ordering: { type: Number, trim: true },
+	created: { type: Date, trim: true, default: Date.now },
 });
 
 const dividerSchema = new mongoose.Schema({
-	dividerName: { type: String, required: true },
+	dividerName: { type: String, trim: true, required: true },
 	folders: [folderSchema],
+	ordering: { type: Number, trim: true },
+	created: { type: Date, trim: true, default: Date.now },
 });
 
 const userSchema = new mongoose.Schema(
 	{
-		firstName: { type: String },
-		lastName: { type: String },
-		username: { type: String, required: true, unique: true },
-		hashedPassword: { type: String, required: true },
+		firstName: { type: String, trim: true },
+		lastName: { type: String, trim: true },
+		username: { type: String, trim: true, required: true, unique: true },
+		hashedPassword: { type: String, trim: true, required: true },
 		dividers: [dividerSchema],
+		created: { type: Date, trim: true, default: Date.now },
 	},
 	{ collection: 'users' },
 );
