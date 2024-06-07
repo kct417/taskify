81fe9fdce757c099d05af0d3600ae9d7b9ead2ff
kct417 <ctran156@calpoly.edu>
2024-06-06 21:29:17 -0700

    Update CORS options for express-backend

--- packages/express-backend/index.js
+++ packages/express-backend/index.js
@@ -7,10 +7,15 @@ import { registerUser, loginUser, authenticateUser } from './auth-user.js';
 
 // available routes for the frontend
 
+const corsOptions = {
+	origin: 'https://thankful-smoke-0f194be1e.5.azurestaticapps.net',
+	optionsSuccessStatus: 200,
+};
+
 const app = express();
 const port = 8000;
 
-app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 
 app.post('/signup', registerUser);
